21a0d555c7af44f97f57ce47f5611b693cf427b8
SuperStar0106 <jarikivi0106@gmail.com>
2023-10-18 19:30:08 +0200

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { MainPage } from "./pages";
 import { PATH } from "./consts";
+import { darkTheme } from "./styles";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={PATH.MAIN} element={<MainPage />}></Route>
-        <Route path="*" element={<Navigate to={PATH.MAIN} />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={PATH.MAIN} element={<MainPage />}></Route>
+          <Route path="*" element={<Navigate to={PATH.MAIN} />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
